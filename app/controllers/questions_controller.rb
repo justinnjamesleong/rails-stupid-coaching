26731ca01f12b5c2634df39950e4ca37af4740fc
justinn leong <justinnjamesleong@gmail.com>
2022-05-24 12:33:33 +0800

    skeleton done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = ['Great!', 'Silly question, get dressed and go to work!.', "I don't care, get dressed and go to work!"]
+
+    if params[:question] == "I am going to work"
+      @answer = @answer[0]
+    elsif params[:question].include? "?"
+      @answer = @answer[1]
+    else
+      @answer = @answer[2]
+    end
+  end
+end
